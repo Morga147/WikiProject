d6246ea3ffb5d2a35e198a5d8836d67e02350a1c
Joy Morgan <Tesoro1021@gmail.com>
2015-09-21 15:45:54 -0400

    CSS updates

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -4,11 +4,16 @@ require 'capybara/rspec'
 
 RSpec.describe 'Login Feature', type: :feature do
   it "works" do
+    #checks to see root path works
     visit '/'
+    #checks to see login link works
     click_link 'Log In'
+    #checks if page title is what it's supposed to be
+    expect(page.title).to eq 'Vernaculasaurus'
+    
     # binding.pry drops you into a rails console after
     # the test is done running (well, drops you in wherever the
     # binding.pry is located in your test)
-    binding.pry
+    # binding.pry
   end #end it "works"
 end #end RSpec.describe
